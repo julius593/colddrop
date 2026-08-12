d2e20cca3bc542338259e4b95aaed29824405ba3
julius <floresbazoaltojuliogabriel9@gmail.com>
2026-08-12 15:31:47 -0400

    ert

--- usuarios/casaleerusuarios.php
+++ usuarios/casaleerusuarios.php
@@ -1,23 +1,23 @@
-<label for="CI">CI</label>
-<input type="number" name="CI" id="CI">
+<?php
+// ========================================================
+// VER DETALLES DE USUARIO (CASALEERUSUARIOS.PHP)
+// ========================================================
+include_once '../conexion.php';
 
-<label for="Nombre">Nombre</label>
-<input type="text" name="Nombre" id="Nombre"> 
+if (!headers_sent() && session_status() === PHP_SESSION_NONE) {
+    @session_start();
+}
 
-<label for="Apellido">Apellido</label>
-<input type="text" name="Apellido" id="Apellido">
+$CI = isset($_GET['CI']) ? $_GET['CI'] : (isset($_GET['idCI']) ? $_GET['idCI'] : '');
+$usuarioData = null;
 
-<label for="Usuario">Usuario</label>
-<input type="text" name="Usuario" id="Usuario">
-
-<label for="Contrasena">Contraseña</label>
-<input type="password" name="Contrasena" id="Contrasena">
-
-<label for="Direccion">Direccion</label >
-<input type="text" name="Direccion" id="Direccion">
-
-<label for="Celular">Celular</label>
-<input type="number" name="Celular"id="Celular" >
+if (!empty($CI)) {
+    $sql = "SELECT * FROM usuarios WHERE CI = '$CI'";
+    $resultado = $conexion->query($sql);
+    if ($resultado && $resultado->num_rows > 0) {
+        $usuarioData = $resultado->fetch_assoc();
+    }
+}
 
 ?>
 <!DOCTYPE html>
