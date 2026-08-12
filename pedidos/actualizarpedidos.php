9b2ff04738623e66234f2054cec7b4b8f6facb74
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-12 15:15:59 -0400

    e

--- pedidos/actualizarpedidos.php
+++ pedidos/actualizarpedidos.php
@@ -15,32 +15,10 @@ if($resultado->num_rows>0){
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Como hacer un CRUD</title>
-</head>
-<body>
-    <form action="casaactualizarpedidos.php" method="post">
-<h2>PEDIDOS</h2>
-<label for="">ID de PEDIDOS</label>
-<input type="number" name="idPEDIDOS" value='<?=$idPEDIDOS?>'>
-<label for="">Nombre</label>
-<input type="text" name="Nombre" value='<?=$Nombre?>'> 
-
-<label for="">Fecha</label>
-<input type="text" name="Fecha" value='<?=$Fecha?>'>
-
-<label for="">Estado</label>
-<input type="number" name="Estado" value='<?=$Estado?>'>
-
-<label for="">NombreVendedor</label>
-<input type="text" name="NombreVendedor" value='<?=$NombreVendedor?>'>
-
-<input type="submit" >
-    </form>
-    
-</body>
-</html>
+if($conn->query($sql) === TRUE){
+    header("location:../princip/micarrito.php?idPEDIDOS=".$idPEDIDOS);
+} else{
+    echo"Error:". $sql. "<br>". $conn->error;
+}
+$conn->close();
+?>
