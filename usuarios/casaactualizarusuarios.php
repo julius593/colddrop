0fc2707a9a76bbd8da559dc4cebcc6344ebeac7a
julius <floresbazoaltojuliogabriel9@gmail.com>
2026-08-12 15:28:31 -0400

    qw

--- usuarios/casaactualizarusuarios.php
+++ usuarios/casaactualizarusuarios.php
@@ -1,22 +1,14 @@
-<?php 
+<?php
 include_once '../conexion.php';
-
 $CI=$_POST['CI'];
 $Nombre=$_POST['Nombre'];
-$Apellido=$_POST['Apellido'];
-$Usuario=$_POST['Usuario'];
-$Contrasena=$_POST['Contrasena'];
 $Direccion=$_POST['Direccion'];
 $Celular=$_POST['Celular'];
 $Rol=$_POST['Rol'];
 $Estado=$_POST['Estado'];
-$sql = "INSERT INTO usuarios (CI,Nombre, Apellido, Usuario, Contrasena, Direccion, Celular, Rol, Estado ) VALUES('$CI','$Nombre','$Apellido','$Usuario','$Contrasena','$Direccion','$Celular', '$Rol','$Estado' )";
 
-if($conn->query($sql) === TRUE){
-    echo"Nuevo usuario creado exitosamente";
-} else{
-    echo"Error:". $sql. "<br>". $conn->error;
-    
+$sql="UPDATE usuarios SET Nombre='$Nombre',Direccion='$Direccion',Celular='$Celular',Rol='$Rol',Estado='$Estado',CI='$CI' WHERE CI=$CI ";
+if($conexion->query($sql)=== TRUE){
+    echo "El usuario se proporciono exitosamente:)";
 }
-$conn->close();
 ?>
