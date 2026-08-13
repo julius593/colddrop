c58bf5fcf2ac032c04afc73a2704fb2625364fff
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:16:37 -0400

    vf

--- princip/eliminar_ajax.php
+++ princip/eliminar_ajax.php
@@ -8,32 +8,3 @@ header('Content-Type: application/json');
 if (!headers_sent() && session_status() === PHP_SESSION_NONE) {
     @session_start();
 }
-
-$codigo = isset($_POST['Codigo']) ? $_POST['Codigo'] : '';
-$idPedido = isset($_POST['idPedido']) ? $_POST['idPedido'] : '';
-
-if (empty($codigo) || empty($idPedido)) {
-    echo json_encode(["success" => false, "message" => "Datos incompletos"]);
-    exit();
-}
-
-$sqlDel = "DELETE FROM Carrito WHERE PRODUCTOS_Codigo = '$codigo' AND PEDIDOS_idPEDIDOS = '$idPedido'";
-$conn->query($sqlDel);
-
-// Recalcular total del pedido
-$sqlTotal = "SELECT sum(costoTotal) as total FROM Carrito WHERE PEDIDOS_idPEDIDOS = '$idPedido'";
-$resTotal = $conn->query($sqlTotal);
-$totalRow = $resTotal->fetch_assoc();
-$total = isset($totalRow['total']) && $totalRow['total'] !== null ? (float)$totalRow['total'] : 0;
-
-// Verificar si quedan prendas en el carrito
-$sqlCart = "SELECT count(*) as cant FROM Carrito WHERE PEDIDOS_idPEDIDOS = '$idPedido'";
-$resCart = $conn->query($sqlCart);
-$cantItems = $resCart ? $resCart->fetch_assoc()['cant'] : 0;
-
-echo json_encode([
-    "success" => true,
-    "total" => number_format($total, 2),
-    "cantItems" => $cantItems
-]);
-?>
