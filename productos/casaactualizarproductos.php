1d966713b46334807dee971ba1bb092085762c52
mateommmsejas <cruzgoytiaenrique@pedropoveda.edu.bo>
2026-08-13 12:01:48 -0400

    elim

--- productos/casaactualizarproductos.php
+++ productos/casaactualizarproductos.php
@@ -4,14 +4,6 @@
 // ========================================================
 include_once '../conexion.php';
 
-$Codigo = isset($_POST['Codigo']) ? $_POST['Codigo'] : '';
-$Nombre = isset($_POST['Nombre']) ? $_POST['Nombre'] : '';
-$Tipo = isset($_POST['Tipo']) ? $_POST['Tipo'] : '';
-$Talla = isset($_POST['Talla']) ? $_POST['Talla'] : '';
-$Color = isset($_POST['Color']) ? $_POST['Color'] : '';
-$Costo = isset($_POST['Costo']) ? $_POST['Costo'] : '0';
-$Stock = isset($_POST['Stock']) ? $_POST['Stock'] : '0';
-$Imagen = isset($_POST['Imagen']) ? $_POST['Imagen'] : '';
 
 // Procesar nuevo archivo de imagen si se subió uno
 if (isset($_FILES['ImagenFile']) && $_FILES['ImagenFile']['error'] === UPLOAD_ERR_OK) {
