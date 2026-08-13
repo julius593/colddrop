1d7f47a56141f100ca51a0a7a239302e3e59f7ad
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:02:50 -0400

    pppp

--- pedidos/casaactualizarpedidos.php
+++ pedidos/casaactualizarpedidos.php
@@ -5,3 +5,6 @@ $Estado=$_POST['Estado'];
 $NombreVendedor=$_POST['NombreVendedor'];
 $sql="UPDATE PEDIDOS SET Nombre='$Nombre',Fecha='$Fecha',Estado='$Estado',NombreVendedor='$NombreVendedor' WHERE idPEDIDOS=$idPEDIDOS ";
 if($conexion->query($sql)=== TRUE){
+    echo "El pedido se proporciono exitosamente:)";
+}
+?>
