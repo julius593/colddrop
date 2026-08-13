e1928ab0757e9fa66793f8ea430b3851bf452530
AritzPS <venegasmarchantsebastian@pedropoveda.edu.bo>
2026-08-13 11:56:50 -0400

    arreglos foot

--- princip/footer.php
+++ princip/footer.php
@@ -12,38 +12,7 @@
 </head>
 <body>
     <footer>
-        <!-- Sección 1: Logo -->
-        <div>
-            <a href="inicio.php">
-                <img id="logoh" src="../imagenes/Captura_de_pantalla_2026-04-23_121404-removebg-preview.png" alt="ColdDrop Logo" style="width: 150px;">
-            </a>
-        </div>
 
-        <!-- Sección 2: Menú de productos -->
-        <div>
-            <h4 style="margin-bottom: 10px;">Comprar</h4>
-            <li style="list-style: none;"><a class="listafooter" href="hoodeis.php">Hoodies</a></li>
-            <li style="list-style: none;"><a class="listafooter" href="poleras.php">Poleras</a></li>
-            <li style="list-style: none;"><a class="listafooter" href="pantalones.php">Pantalones</a></li>
-        </div>
-
-        <!-- Sección 3: Contacto -->
-        <div>
-            <h4 style="margin-bottom: 10px;">Comunícate con Nosotros</h4>
-            <li style="list-style: none;"><a class="listafooter" href="mailto:contacto@colddrop.bo">Gmail</a></li>
-            <li style="list-style: none;"><a class="listafooter" href="https://chat.whatsapp.com/FSKwSL02bRd4CEnNvC4PY1" target="_blank">WhatsApp</a></li>
-        </div>
-
-        <!-- Sección 4: Redes sociales -->
-        <div class="social-box">
-            <h4>Síguenos</h4>
-            <div class="social-icons">
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-regular fa-envelope"></i></a>
-            </div>
-        </div>
     </footer>
 </body>
 </html>
