fc0cbf44d096d2b5474cc9db21202f76fb1e7f23
AritzPS <venegasmarchantsebastian@pedropoveda.edu.bo>
2026-08-13 11:54:51 -0400

    advd

--- princip/footer.php
+++ princip/footer.php
@@ -1,23 +1,4 @@
-<!-- ========================================================
-     PIE DE PÁGINA GENERAL (FOOTER)
-     ======================================================== -->
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Hoja de estilos externa para el pie de página -->
-    <link rel="stylesheet" href="../css/footer.css">
-</head>
-<body>
-    <footer>
-        <!-- Sección 1: Logo -->
-        <div>
-            <a href="inicio.php">
-                <img id="logoh" src="../imagenes/Captura_de_pantalla_2026-04-23_121404-removebg-preview.png" alt="ColdDrop Logo" style="width: 150px;">
-            </a>
-        </div>
+
 
         <!-- Sección 2: Menú de productos -->
         <div>
