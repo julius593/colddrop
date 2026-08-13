30f2daa93bba79f9e0605a7b2c32261504d07d19
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:08:31 -0400

    jhj

--- pedidos/casacrearpedidos.php
+++ pedidos/casacrearpedidos.php
@@ -7,3 +7,5 @@ $NombreVendedor=$_POST['NombreVendedor'];
 $sql = "INSERT INTO pedidos (idPEDIDOS, Nombre, Fecha, Estado, NombreVendedor) VALUES('$idPEDIDOS','$Nombre','$Fecha', '$Estado','$NombreVendedor' )";
 if($conn->query($sql) === TRUE){
     header("location:../princip/micarrito.php?idPEDIDOS=".$idPEDIDOS);
+    } else{
+    echo"Error:". $sql. "<br>". $conn->error;
