a4faeb923eb0a3c29dd29ab0e7109a9233a7311c
julius <floresbazoaltojuliogabriel9@gmail.com>
2026-08-13 12:01:59 -0400

    cbvb

--- usuarios/leerusuarios.php
+++ usuarios/leerusuarios.php
@@ -114,14 +114,3 @@ $resultado = $conexion->query($sql);
 
 
 
-<?php
-include_once '../conexion.php';
-$idCI=$_GET['idCI'];
-$sql="DELETE FROM usuarios WHERE CI=$CI";
-if($conn->query($sql)===TRUE) {
-    echo"nuevos usuarios eliminado exitosamente";
-} else {
-    echo"error:".$sql."<br>";
-}
-
-?>
