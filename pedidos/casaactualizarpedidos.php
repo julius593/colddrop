bf66972d9a6dbd3d076ba0b132693aac0247522e
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:00:37 -0400

    ww

--- pedidos/casaactualizarpedidos.php
+++ pedidos/casaactualizarpedidos.php
@@ -1,7 +1,5 @@
 <?php
 include_once '../conexion.php';
-$idPEDIDOS=$_POST['idPEDIDOS'];
-$Nombre=$_POST['Nombre'];
-Nombre=$_POST['Nombre'];
 $Fecha=$_POST['Fecha'];
 $Estado=$_POST['Estado'];
+$NombreVendedor=$_POST['NombreVendedor'];
