a5ed2efecbc48b30e659526d894c54bf85c00167
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:22:06 -0400

    v

--- princip/eliminar_venta.php
+++ princip/eliminar_venta.php
@@ -7,20 +7,3 @@ include_once '../conexion.php';
 if (!headers_sent() && session_status() === PHP_SESSION_NONE) {
     @session_start();
 }
-
-// Verificación estricta de permisos de Administrador
-if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'Administrador') {
-    header("Location: leer_ventas.php");
-    exit();
-}
-
-$idVenta = isset($_GET['idVenta']) ? $_GET['idVenta'] : '';
-
-if (!empty($idVenta)) {
-    $sqlDel = "DELETE FROM ventas WHERE idVenta = '$idVenta'";
-    $conn->query($sqlDel);
-}
-
-header("Location: leer_ventas.php");
-exit();
-?>
