5d98289f8854b7580028859ea2fcaf9f73c34a2f
fabiana <mendozamoralessantiago@pedropoveda.edu.bo>
2026-08-13 12:11:10 -0400

    fd

--- princip/elimcarrito.php
+++ princip/elimcarrito.php
@@ -10,16 +10,4 @@ include_once '../conexion.php';
 $codigo = isset($_GET['Codigo']) ? $_GET['Codigo'] : '';
 $idPedido = isset($_GET['idPedido']) ? $_GET['idPedido'] : (isset($_GET['idPEDIDOS']) ? $_GET['idPEDIDOS'] : '');
 
-// 3. Si ambos datos existen, eliminamos la fila correspondiente de la tabla Carrito
-if (!empty($codigo) && !empty($idPedido)) {
-    $codigo = $conn->real_escape_string($codigo);
-    $idPedido = (int)$idPedido;
 
-    $sql = "DELETE FROM Carrito WHERE PRODUCTOS_Codigo = '$codigo' AND PEDIDOS_idPEDIDOS = $idPedido";
-    $conn->query($sql);
-}
-
-// 4. Redirigimos al usuario de vuelta a su carrito para ver la lista actualizada
-header("Location: micarrito.php?idPedido=" . $idPedido);
-exit;
-?>
