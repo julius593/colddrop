0c75c719394489380929715a63eef897bdfea105
julius <floresbazoaltojuliogabriel9@gmail.com>
2026-08-28 12:17:21 -0400

    ccvx

--- rp/ingresostotales.php
+++ rp/ingresostotales.php
@@ -6,9 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Reportes</h1>
+    <div class="form-container" style="margin: 40px auto;">
+        <label for="">Por DIA</label>
+        <input type="text">
+    </div>
     <div>
-        <label for=""></label>
+        <label for="">Por SEMANA</label>
+        <input type="text">
+    </div>
+     <div>
+        <label for="">Por MES</label>
+        <input type="text">
+    </div>
+     <div>
+        <label for="">Por AÑO</label>
         <input type="text">
     </div>
 </body>
