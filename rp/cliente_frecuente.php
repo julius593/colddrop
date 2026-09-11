7476fbd199d1c9a1161aff54beef6b3a66253a79
tio <mendozamoralessantiago@pedropoveda.edu.bo>
2026-09-11 12:12:51 -0400

    Update cliente_frecuente.php

--- rp/cliente_frecuente.php
+++ rp/cliente_frecuente.php
@@ -14,3 +14,19 @@ if (!isset($_SESSION['rol']) || ($_SESSION['rol'] != 'Administrador' && $_SESSIO
     header("Location: ../princip/iniciosesion.php");
     exit();
 }
+<?php
+// ========================================================
+// REPORTE: CLIENTE MÁS FRECUENTE - COLDDROP
+// ========================================================
+include_once '../conexion.php';
+
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+if (!isset($_SESSION['rol']) || ($_SESSION['rol'] != 'Administrador' && $_SESSION['rol'] != 'vendedor')) {
+    header("Location: ../princip/iniciosesion.php");
+    exit();
+}
